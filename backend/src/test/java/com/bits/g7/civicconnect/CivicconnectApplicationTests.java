24e23568933700c768e37f9807be2ad395ff748d
aryanavnish <aryan.avnish@valtech.com>
2026-02-12 15:33:28 +0530

    Initial commit for backend structure

---
+++ backend/src/test/java/com/bits/g7/civicconnect/CivicconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.g7.civicconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
